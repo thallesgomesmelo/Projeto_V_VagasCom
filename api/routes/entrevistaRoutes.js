ad400849def4bfa16aea26a77f58bf0e00e0fcef
Adrianafon <adrianalino.bh@gmail.com>
2021-10-10 22:13:41 -0300

    Update entrevistaRoutes.js

--- api/routes/entrevistaRoutes.js
+++ api/routes/entrevistaRoutes.js
@@ -1,5 +1,23 @@
 module.exports = function(app) {
     const entrevistaController = require('../controllers/entrevistaController')
+    const jwt = require('jsonwebtoken')
+    const SECRET = 'tarley'
+
+    verifyJWT = (req, res, next) => {
+        const token = req.headers['x-access-token']
+        if(!token) {
+            return res.status(401).json({ auth: false, message: 'Token não informado.' })
+        }
+        
+        jwt.verify(token, SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(500).json({ auth: false, message: 'Erro: Token não autenticado.' })
+            }
+
+            req.userId = decoded.id
+            next()
+        })
+    }    
 
     app.route('/entrevista')
         .get(entrevistaController.lista_de_todas_as_entrevistas)
