4f9837fd8307110f9959990a41e772dda7262389
unknown <thallesgomesmello@gmail.com>
2021-10-12 14:11:41 -0300

    Adicionado o jwt na entrevistaRoutes

--- api/routes/entrevistaRoutes.js
+++ api/routes/entrevistaRoutes.js
@@ -1,8 +1,27 @@
 module.exports = function(app) {
     const entrevistaController = require('../controllers/entrevistaController')
+    const jwt = require('jsonwebtoken')    
+    const SECRET = 'tarley'
+
+    
+    verifyJWT = (req, res, next) => {
+        const token = req.headers['x-access-token']
+        if(!token) {
+            return res.status(401).json({ auth: false, message: 'Token não informado.' })
+        }
+        
+        jwt.verify(token, SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(500).json({ auth: false, message: 'Erro: Token não autenticado.' })
+            }
+
+            req.userId = decoded.id
+            next()
+        })
+    }
 
     app.route('/entrevista')
-        .get(entrevistaController.lista_de_todas_as_entrevistas)
+        .get(verifyJWT, entrevistaController.lista_de_todas_as_entrevistas)
         .post(entrevistaController.adiciona_uma_entrevista)
 
     app.route('/entrevista/:entrevistaId')
